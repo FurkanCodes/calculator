1c556c1f81a1ad103a2eacf968dd383273770847
Furkan <turkogluvelifurkan@gmail.com>
2021-04-07 20:20:00 +0300

    heyt

--- app.js
+++ app.js
@@ -1,83 +1,100 @@
-// function add(a, b) {
-//     return a + b;
-//   }
+function add(a, b) {
+    return parseFloat(a) + (b);
+  }
   
-//   function subtract(a, b) {
-//     return a - b;
-//   }
+  function subtract(a, b) {
+    return parseFloat(a) - (b);
+  }
   
-//   function multiply(a, b) {
-//     return a * b;
-//   }
+  function multiply(a, b) {
+    return parseFloat(a) * parseFloat(b);
+  }
   
-//   function divide(a, b) {
-//     return a / b;
-//   }
+  function divide(a, b) {
+    if (b != '0'){
+        return parseFloat(a) / parseFloat(b)
+        } else {
+           return "I afraid you cannot divide by 0"; 
+        }
+  }
   
   
-//   function calculate(a,operator,b) {
-//       if(operator === '+') {
-//           return add(a, b);
-//       } else if(operator === '-'){
-//           return subtract(a, b);
-//       } else if(operator === '*') {
-//           return multiply(a, b);
-//       } else if(operator === '/'){
-//           return divide(a, b);
-//       }
-      
-//   }
+  function calculate(a,operator,b) {
+      if(operator === '+') {
+          return add(a, b);
+      } else if(operator === '-'){
+          return subtract(a, b);
+      } else if(operator === 'x') {
+          return multiply(a, b);
+      } else if(operator === '/') {
+        return divide(a, b);
+      }
+          
+  }
+
   const calculator = document.querySelector('.calculator');
   const numButton = document.querySelectorAll('.number');
   const opButton = document.querySelectorAll('.operator');
   const clear = document.querySelector('.clear');
   const display = document.querySelector('.calc_display');
   const keyPad = document.querySelector('.calc_keys');
+const equalsKey = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal');
+
+let displayVal = '';
+let clickedOperator = ''
+let firstNumber = '';
+let result = '';
+display.textContent = 0;
 
 
- 
- function getDisplay(){
-   return display.innerText;
- }
+
+numButton.forEach(number => {
+ number.addEventListener('click', function() {
+  displayVal += number.id;
+     display.textContent = displayVal;
+ })})
+
+opButton.forEach((operator => {
+  operator.addEventListener('click', function() {
+    if (firstNumber && displayVal) {
+      displayResult();
+    }
+    firstNumber = displayVal;
+    clickedOperator = operator.textContent;
+    display.textContent = displayVal + clickedOperator;
+    displayVal = '';
+  })
+}));
+
+equalsKey.addEventListener('click', function() {
+ displayResult();
+})
 
 
- function printDisplay(num) {
-  if(num === ''){
-    display.innerText = num;
-  } else {
-    display.innerText = getNumber(num);
-  }
- }
 
-
- function getNumber(num) {
-   let n = Number(num);
-   let value = n.toLocaleString("en");
-   return value;
- }
-
-
- function reverseNumberFormat(num){
-	return Number(num.replace(/,/g,''));
+function displayResult() {
+  result = calculate(parseFloat(firstNumber), clickedOperator, parseFloat(displayVal));
+  display.textContent = result;
+  display.textContent = firstNumber +  displayVal;
+  displayVal = result;
 }
 
 
 
-for(let i = 0; i < opButton.length; i++) {
-  opButton[i].addEventListener('click', function(){
-  
-	});
+// clears the display
+clearBtn.addEventListener('click', function() {
+ return resetAll();
+})
+
+
+function resetAll(){
+    displayVal = '';
+    clickedOperator = ''
+    firstNumber = '';
+    result = '';
+    display.textContent = 0;
 }
 
-
-
-for(var i =0;i<numButton.length;i++){
-	numButton[i].addEventListener('click',function(){
-		var output=reverseNumberFormat(getDisplay());
-		if(output!=NaN){ //if output is a number
-			output=output+this.id;
-			printDisplay(output);
-		}
-	});
-}
 
