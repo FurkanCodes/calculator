213279a9ec52434e9a7ec4b2944a57d59a72fa26
Furkan <turkogluvelifurkan@gmail.com>
2021-04-08 12:06:03 +0300

    hey

--- app.js
+++ app.js
@@ -1,100 +1,112 @@
+
 function add(a, b) {
-    return parseFloat(a) + (b);
-  }
-  
-  function subtract(a, b) {
-    return parseFloat(a) - (b);
-  }
-  
-  function multiply(a, b) {
-    return parseFloat(a) * parseFloat(b);
-  }
-  
-  function divide(a, b) {
-    if (b != '0'){
-        return parseFloat(a) / parseFloat(b)
-        } else {
-           return "I afraid you cannot divide by 0"; 
-        }
-  }
-  
-  
-  function calculate(a,operator,b) {
-      if(operator === '+') {
-          return add(a, b);
-      } else if(operator === '-'){
-          return subtract(a, b);
-      } else if(operator === 'x') {
-          return multiply(a, b);
-      } else if(operator === '/') {
-        return divide(a, b);
-      }
-          
-  }
+  return parseFloat(a) + b;
+}
 
-  const calculator = document.querySelector('.calculator');
-  const numButton = document.querySelectorAll('.number');
-  const opButton = document.querySelectorAll('.operator');
-  const clear = document.querySelector('.clear');
-  const display = document.querySelector('.calc_display');
-  const keyPad = document.querySelector('.calc_keys');
-const equalsKey = document.querySelector('.equal');
-const clearBtn = document.querySelector('.clear');
-const decimal = document.querySelector('.decimal');
+function subtract(a, b) {
+  return parseFloat(a) - b;
+}
 
-let displayVal = '';
-let clickedOperator = ''
-let firstNumber = '';
-let result = '';
+function multiply(a, b) {
+  return parseFloat(a) * parseFloat(b);
+}
+
+function divide(a, b) {
+  if (b != "0") {
+    return parseFloat(a) / parseFloat(b);
+  } else {
+    return "I afraid you cannot divide by 0";
+  }
+}
+
+function calculate(a, operator, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "x") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  }
+}
+
+const calculator = document.querySelector(".calculator");
+const numButton = document.querySelectorAll(".number");
+const opButton = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const display = document.querySelector(".calc_display");
+const keyPad = document.querySelector(".calc_keys");
+const equalsKey = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const decimal = document.querySelector(".decimal");
+
+let displayVal = "";
+let clickedOperator = "";
+let firstNumber = "";
+let result = "";
+
+// Decimal counter
+let decimalCount = 0;
+
 display.textContent = 0;
 
+numButton.forEach((number) => {
+  number.addEventListener("click", function () {
+    // For  the decimal increment decimal counter
+    if (number.id === ".") {
+      decimalCount++;
+    }
 
+    // For more than one decimal don't do anything. Return
+    if (number.id === "." && decimalCount > 1) {
+      return;
+    }
 
-numButton.forEach(number => {
- number.addEventListener('click', function() {
-  displayVal += number.id;
-     display.textContent = displayVal;
- })})
+    displayVal += number.id;
+    display.textContent = displayVal;
+  });
+});
 
-opButton.forEach((operator => {
-  operator.addEventListener('click', function() {
+opButton.forEach((operator) => {
+  operator.addEventListener("click", function () {
     if (firstNumber && displayVal) {
       displayResult();
     }
     firstNumber = displayVal;
+    decimalCount = 0;
     clickedOperator = operator.textContent;
     display.textContent = displayVal + clickedOperator;
-    displayVal = '';
-  })
-}));
+    displayVal = "";
+  });
+});
 
-equalsKey.addEventListener('click', function() {
- displayResult();
-})
-
-
+equalsKey.addEventListener("click", function () {
+  displayResult();
+});
 
 function displayResult() {
-  result = calculate(parseFloat(firstNumber), clickedOperator, parseFloat(displayVal));
+  result = calculate(
+    parseFloat(firstNumber),
+    clickedOperator,
+    parseFloat(displayVal)
+  );
   display.textContent = result;
-  display.textContent = firstNumber +  displayVal;
+  display.textContent = firstNumber + displayVal;
   displayVal = result;
 }
 
-
-
 // clears the display
-clearBtn.addEventListener('click', function() {
- return resetAll();
-})
+clearBtn.addEventListener("click", function () {
+  return resetAll();
+});
 
-
-function resetAll(){
-    displayVal = '';
-    clickedOperator = ''
-    firstNumber = '';
-    result = '';
-    display.textContent = 0;
+function resetAll() {
+  displayVal = "";
+  clickedOperator = "";
+  firstNumber = "";
+  result = "";
+  display.textContent = 0;
 }
 
 
